19463893f6a1857d55d76b67a4571f218f7d9ed8
Vlad Anuchin <vlad.anuchin@gmail.com>
2020-08-28 00:26:17 +0200

    0.613

--- src/env.js
+++ src/env.js
@@ -4,7 +4,7 @@
     // API url:port
     window.__env.apiUrl = 'http://webapi.server.com';
     // app version
-    window.__env.version = '0.612';
+    window.__env.version = '0.613';
   
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
